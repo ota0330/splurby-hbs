b4f060f9d370e15ac55de283f862ab3842434011
ota0330 <ota@gmail.com>
2014-10-04 18:52:00 +0000

    we added teh simple from and then we did the launch bottu

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,20 @@ class QuotesController < ApplicationController
 		@quote = Quote.order("RANDOM()").last
 	end
 
+	def new
+		@quote = Quote.new
+	end
+
+	def create
+		Quote.create(quote_params)
+		redirect_to quotes_path
+	end
+
+	private
+
+	def quote_params
+
+		params.require(:quote).permit(:saying, :author)
+	end
+
 end
